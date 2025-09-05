052dc1fcd6dca7286950d4858394b41298cf2073
Oleksy1121 <oleszczyk.dev@gmail.com>
2025-09-05 22:54:18 +0200

    feat: add Snake AI project to portfolio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Portfolio from './components/Portfolio';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SnakeAI from './components/SnakeAI';
 import DigitPredictor from './components/HandwrittenDigitRecognizer/DigitPredctor'
 import TopNavbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -13,6 +14,7 @@ function App() {
     <TopNavbar></TopNavbar>
       <Routes>
         <Route path='/' element={<Portfolio></Portfolio>}></Route>
+        <Route path='/snake-ai' element={<SnakeAI></SnakeAI>}></Route>
         <Route path='/handwritten-digit-recognizer' element={<DigitPredictor></DigitPredictor>}></Route>
       </Routes>
     <Footer></Footer>
