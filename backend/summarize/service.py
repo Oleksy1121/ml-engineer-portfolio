bc6a4c6630f69e32d50f2e86a6c8f980e8b1c6fa
Oleksy1121 <oleszczyk.dev@gmail.com>
2025-10-01 19:24:44 +0200

    feat: add audio file cleanup after summarization process

--- backend/summarize/service.py
+++ backend/summarize/service.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os
+import glob
 from dotenv import load_dotenv
 from openai import OpenAI
 
 from scripts.video_summarizer.download import download_audio, get_video_info, validate_video
 from scripts.video_summarizer.transcribe import get_transcription
 from scripts.video_summarizer.summarizer import get_summarize
+from scripts.video_summarizer.constants import AUDIO_FILENAME
 
 
 load_dotenv()
@@ -22,14 +24,20 @@ async def summarize_service(url: str, send_status):
         await send_status(result)
         raise ValueError(result)
 
-    await send_status("Downloading audio...")
-    download_audio(url)
+    try:
+        await send_status("Downloading audio...")
+        download_audio(url)
 
-    await send_status("Generate Transcription from audio file...")
-    transcription = get_transcription(openai=openai)
+        await send_status("Generate Transcription from audio file...")
+        transcription = get_transcription(openai=openai)
 
-    await send_status("Generatng Summarize...")
-    summary = get_summarize(openai=openai, transcription=transcription)
+        await send_status("Generatng Summarize...")
+        summary = get_summarize(openai=openai, transcription=transcription)
+    
+    finally:
+        audio_folder = os.path.dirname(AUDIO_FILENAME)
+        for file_path in glob.glob(os.path.join(audio_folder, "*")):
+            os.remove(file_path)
 
     await send_status("Finished!")
     return summary
